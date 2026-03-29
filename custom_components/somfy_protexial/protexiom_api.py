821b7068065cfdfbe3c9ff763f413b65988e732b
dric3431 <58148517+dric3431@users.noreply.github.com>
2026-03-29 17:33:43 +0200

    Update protexiom_api.py

--- custom_components/somfy_protexial/protexiom_api.py
+++ custom_components/somfy_protexial/protexiom_api.py
@@ -14,6 +14,7 @@ class ProtexiomApi(AbstractApi):
             Page.CHALLENGE_CARD: "/u_print.htm",
             Page.VERSION: None,
             Page.DEFAULT: "/default.htm",
+            Page.JOURNAL: "/journal.htm",  # Ajouté pour le journal,
         }
         self.selectors = {
             Selector.CONTENT_TYPE: "meta[http-equiv='content-type']",
